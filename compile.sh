3bda82ff4e6cc15eb88b7c5d6357ae45c31ca5b2
catas <catasw@foxmail.com>
2025-04-11 01:59:01 +0800

    gui - version

--- compile.sh
+++ compile.sh
@@ -5,9 +5,8 @@ mvn install
 
 # shellcheck disable=SC2164
 cd gui;
-#export GRAALVM_HOME='/Library/Java/JavaVirtualMachines/graalvm-java23-darwin-aarch64-gluon-23+25.1-dev/Contents/Home';
 export GRAALVM_HOME='/Library/Java/JavaVirtualMachines/graalvm-svm-java17-darwin-m1-gluon-22.1.0.1-Final/Contents/Home';
-export JAVA_HOME=$GRAALVM_HOME
+export JAVA_HOME=$GRAALVM_HOME;
 echo $JAVA_HOME;
 
 cp src/main/resources/graal/darwin/* ./src/main/resources/META-INF.native-image/
